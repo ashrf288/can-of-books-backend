29848eebefa03a3de1fa0386915c7d9dd2376537
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-08-16 17:30:32 +0300

    a

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const jwksClient = require("jwks-rsa");
 
-
-const testController= require('./controllers/test.controller')
 const app = express();
 app.use(cors());
+const PORT = process.env.PORT;
 
-const PORT = process.env.PORT || 3001;
+const client = jwksClient({
+  jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/well/jwks.json`,
+});
+const getKey = (header, callback) => {
+  client.getSigningKey(header.kid, function (err, key) {
+    const signingKey = key.publicKey || key.rsaPublicKey;
+    callback(null, signingKey);
+  });
+};
+app.get("/test", (request, response) => {
+  // TODO:
+  // STEP 1: get the jwt from the headers
+  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
+  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
+  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 
-app.get('/test',testController)
+  const token = request.headers.authorization.split(" ")[1];
+  jwt.verify(token, getKey, {}, (err, user) => {
+    if (err) {
+      response.send("invalid token");
+    }
+    response.send(user);
+  });
+
+  console.log("hello World");
+  response.json({ token: token });
+});
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
